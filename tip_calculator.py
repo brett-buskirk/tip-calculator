b1504c346a98ef3a168800d2b540ec22ad14c40a
Brett Buskirk <buskirkbrett8@gmail.com>
2021-09-30 15:49:27 -0400

    Edited tip calculator to use a function

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,37 +1,22 @@
 # Set static sales tax
 tax = 0.10
 
+# Function to get user input in the correct data type
+def get_user_input(inst_type, msg):
+  user_input = ""
+  while not isinstance(user_input, inst_type):
+    try:
+      user_input = inst_type(input(msg))
+      if user_input <= 0: raise
+    except:
+      print('Please use a positive number only')
+      user_input = ""
+  return user_input
+
 # Declare initial user input variables
-bill = ""
-people = ""
-tip = ""
-
-# Grab and format user input for bill, looping until correct type is given
-while not isinstance(bill, float):
-  try:
-    bill = float(input("How much is the bill? "))
-    if bill <= 0: raise
-  except:
-    print('Please use a positive number only')
-    bill = ""
-
-# Grab and format user input for people, looping until correct type is given
-while not isinstance(people, int):
-  try:
-    people = int(input("How many people are splitting the bill? "))
-    if people <= 0: raise
-  except:
-    print('Please use a positive integer only')
-    people = ""
-
-# Grab and format user input for tip, looping until correct type is given
-while not isinstance(tip, float):
-  try:
-    tip = float(input("What percent will you be tipping? "))
-    if tip <= 0: raise
-  except:
-    print('Please use a positive integer only')
-    tip = ""
+bill = get_user_input(float, "How much is the bill? ")
+people = get_user_input(int, "How many people are splitting the bill? ")
+tip = get_user_input(float, "What percentage will you be tipping? ")
 
 # Set tip as percentage
 tip_percent = tip / 100
@@ -46,15 +31,16 @@ total_bill = bill + tax_amount + tip_amount
 
 # Calculate the total per person
 total_each = total_bill / people
+total_each = "{:.2f}".format(total_each)
 
 # Output the results
-print('\n=======================================')
-print('\t\t\t BILL RESULTS')
-print('=======================================')
+print('\n==========================================')
+print(f'\tBILL RESULTS ({people} people)')
+print('==========================================')
 print(f"Meal Cost: \t\t${bill}")
 print(f"Sales Tax: \t\t${tax_amount}")
 print(f"{tip}% Tip: \t\t${tip_amount}")
-print("=======================================")
+print("==========================================")
 print(f"Total Bill: \t\t${total_bill}")
-print("=======================================")
-print(f"Cost per Person: \t${total_each}")
+print("==========================================")
+print(f"Cost per Person: \t${total_each}\n")
