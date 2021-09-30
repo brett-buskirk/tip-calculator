cf9defc1b4e0993055671877365d0e8e4468c761
Brett Buskirk <buskirkbrett8@gmail.com>
2021-09-30 18:58:57 -0400

    Added detailed comments to the code

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,38 +1,45 @@
 # Set static sales tax
 tax = 0.10
 
-# Function to get user input in the correct data type
+
 def get_user_input(inst_type, msg):
+  """Function to insure user input is in correct data type"""
+  # Make sure user_input is not a number initially
   user_input = ""
+  # Keep prompting user until correct value type is given
   while not isinstance(user_input, inst_type):
     try:
+      # If input can be converted to expected type, continue
       user_input = inst_type(input(msg))
-      if user_input <= 0: raise
+      # If input is equal to or less than 0, raise an exception
+      if user_input <= 0:
+        raise
     except:
+      # If exception is raised, print message and reset user_input
       print('Please use a positive number only')
       user_input = ""
+  # If all is well, return the user input
   return user_input
 
-# Declare initial user input variables
+
+# Declare input variables by passing them through the function
 bill = get_user_input(float, "How much is the bill? ")
 people = get_user_input(int, "How many people are splitting the bill? ")
 tip = get_user_input(float, "What percentage will you be tipping? ")
-
-# Set tip as percentage
-tip_percent = tip / 100
 
 # Calculate the total sales tax
 tax_amount = round(tax * bill, 2)
 
 # Calculate the total tip amount
-tip_amount = round(tip_percent * bill, 2)
+tip_amount = round((tip / 100) * bill, 2)
 
-total_bill = bill + tax_amount + tip_amount
+# Figure the total bill
+total_bill = round(bill + tax_amount + tip_amount, 2)
 
 # Calculate the total per person
-total_each = total_bill / people
+total_each = round(total_bill / people, 2)
 
-# Output the results
+# Output the results in a formatted block
 print('\n==========================================')
 print(f'\tBILL RESULTS ({people} people)')
 print('==========================================')
@@ -42,4 +49,4 @@ print(f"{tip}% Tip: \t\t${tip_amount}")
 print("==========================================")
 print(f"Total Bill: \t\t${total_bill}")
 print("==========================================")
-print(f"Cost per Person: \t${round(total_each, 2)}\n")
+print(f"Cost per Person: \t${total_each}\n")
